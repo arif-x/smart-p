88731cd9d2036ae9b3ad6dff4e2075777e64edcd
Arif-X <ariffudinnotresponding@gmail.com>
2022-03-16 13:28:57 +0700

    Add Template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>NobleUI Responsive Bootstrap 4 Dashboard Template</title>
+	<!-- core:css -->
+	<link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+	<!-- endinject -->
+	<!-- plugin css for this page -->
+	<link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
+	<!-- end plugin css for this page -->
+	<!-- inject:css -->
+	<link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+	<!-- endinject -->
+	<!-- Layout styles -->  
+	<link rel="stylesheet" href="{{ asset('assets/css/demo_1/style.css') }}">
+	<!-- End layout styles -->
+	<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}"/>
+	<script src="{{ asset('assets/vendors/jquery.flot/jquery.flot.js') }}"></script>
+	<script src="{{ asset('assets/vendors/jquery.flot/jquery.flot.resize.js') }}"></script>
+</head>
+<body>
+	<div class="main-wrapper">
+		<div class="page-wrapper">
+			<!--Page Navbar Start-->
+			@include('layouts.template.navbar')
+			<!--Page Navbar Ends-->
+			<!--Page Sidebar Start-->
+			@include('layouts.template.sidebar')
+			<!--Page Sidebar Ends-->
+			<!--Page Content Start-->
+			@yield('content')
+			<!--Page Content Ends-->
+			<!--Page Footer Start-->
+			@include('layouts.template.footer')
+			<!--Page Footer Ends-->
+		</div>
+	</div>
+
+	<!-- core:js -->
+	<script src="{{ asset('assets/vendors/core/core.js') }}"></script>
+	<!-- endinject -->
+	<!-- plugin js for this page -->
+	<script src="{{ asset('assets/vendors/chartjs/Chart.min.js') }}"></script>
+	<script src="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+	<script src="{{ asset('assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+	<script src="{{ asset('assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
+	<!-- end plugin js for this page -->
+	<!-- inject:js -->
+	<script src="{{ asset('assets/vendors/feather-icons/feather.min.js') }}"></script>
+	<script src="{{ asset('assets/js/template.js') }}"></script>
+	<!-- endinject -->
+	<!-- custom js for this page -->
+	<script src="{{ asset('assets/js/dashboard.js') }}"></script>
+	<script src="{{ asset('assets/js/datepicker.js') }}"></script>
+	<!-- end custom js for this page -->
+</body>
+</html>   
+
